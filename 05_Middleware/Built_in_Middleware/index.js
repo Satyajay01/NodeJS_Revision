cf64beab4e635972e3c3838b19acec01f71c841e
satyajay01 <satyajaydibya0@gmail.com>
2025-01-18 14:21:53 +0530

    form control and Built_in_Middleware

---
+++ 05_Middleware/Built_in_Middleware/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app=express();
+app.set("view engine",'ejs')
+
+// this is Built-in Middleware for convert data to json formate
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req, res)=>{
+    res.render("index")
+})
+app.post("/get-form-data",(req, res)=>{
+    console.log(req.body);
+    res.send("Form data received")
+    
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
